84f3907e8760fe64e4f57a42d803b08b823d998e
Nagendra P Sbr <Nagendra.Prasad@in.ey.com>
2020-07-03 11:38:43 +0530

    fix

--- src/webparts/helloWorld/components/ProjectDetails.tsx
+++ src/webparts/helloWorld/components/ProjectDetails.tsx
@@ -13,15 +13,17 @@ const ProjectDetails: React.FC<any> = () => {
 
     const handleProjectTileChange = useConstCallback(
         (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
-            console.log(` handleProjectTileChange newValue : ${newValue}`);
-            setBusinessFormData({...businessFormData, 'projectTitle' : newValue});
+            const newData = businessFormData;
+            newData['projectTitle'] = newValue;
+            setBusinessFormData({...businessFormData, ...newData});
         },
     );
 
     const handleSubmitedByChange = useConstCallback(
         (event: React.FormEvent<HTMLInputElement | HTMLTextAreaElement>, newValue?: string) => {
-           console.log(` handleSubmitedByChange newValue : ${newValue}`);
-           setBusinessFormData({...businessFormData, 'submitedBy' : newValue});
+            const newData = businessFormData;
+            newData['submitedBy'] = newValue;
+            setBusinessFormData({...businessFormData, ...newData});
         },
     );
 
@@ -29,37 +31,20 @@ const ProjectDetails: React.FC<any> = () => {
 
     return (
         <table>
-          <tbody>
-          <tr>
-            <td>Project Title</td>
-            <td><TextField value={businessFormData.projectTitle} name="projectTilte" onChange={handleProjectTileChange} styles={textFieldStyles} required /></td>   
-          </tr>
-          <tr>
-            <td>SUBMITTED BY</td>
-            <td><TextField value={businessFormData.submitedBy} name="submitedBy" onChange={handleSubmitedByChange} styles={textFieldStyles} required/></td>
-          </tr>
-          {/* <td>PROJECT TYPE</td>
-            <td><TextField value={this.state.ProjectType} onChanged={this.ProjectTypeOnchanged.bind(this)} required/></td>   
-          </tr>
-          <tr>
-            <td>PHONE / EMAIL</td>
-            <td><TextField onChanged={this.PhoneEmailOnchanged.bind(this)} value={this.state.PhoneEmail} /></td>
-            <td>TOTAL ESTIMATED BUDGET
-            </td>
-            <td><TextField onChanged={this.TotalBudgetOnchanged.bind(this)} value={this.state.TotalBudget} required /></td>   
-          </tr>
-          <tr>
-            <td>DATE OF PROPOSAL</td>
-            <td><TextField onChanged={this.ProposalDateOnchanged.bind(this)} value={this.state.ProposalDate} required /></td>
-            <td>REQUEST TYPE</td>
-            <td><TextField onChanged={this.RequestTypeOnchanged.bind(this)} value={this.state.RequestType} required /></td>   
-          </tr>
-          <tr>
-            <td>PROJECTED START DATE</td>
-            <td><TextField onChanged={this.ProjStartDateOnchanged.bind(this)} value={this.state.ProjStatrtDate} required /></td>
-            <td>PROJECTED COMPLETION DATE</td>
-            <td><TextField onChanged={this.ProjEndDateOnchanged.bind(this)} value={this.state.ProjCompletionDate} required /></td>   */}
-           </tbody> 
+            <tbody>
+                <tr>
+                    <td>PROJECT TITLE</td>
+                    <td>
+                        <TextField value={businessFormData.projectTitle} name="projectTilte" onChange={handleProjectTileChange} styles={textFieldStyles}  />
+                    </td>   
+                </tr>
+                <tr>
+                    <td>SUBMITTED BY</td>
+                    <td>
+                        <TextField value={businessFormData.submitedBy} name="submitedBy" onChange={handleSubmitedByChange} styles={textFieldStyles} />
+                    </td>
+                </tr>
+            </tbody> 
       </table>
     );
 };
